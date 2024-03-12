9291458bc256136c7bef4c491ab97efeed56d8d5
Zeroum2345 <marcuspsferreira@gmail.com>
2024-03-11 23:04:09 -0300

    Json para persistencia de dados

--- app/Modules/Cadastro.hs
+++ app/Modules/Cadastro.hs
@@ -1,10 +1,33 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Modules.Cadastro where
+
+import GHC.Generics
+import qualified Data.ByteString.Lazy as B
+import Data.Aeson
 
 cadastroProfessor :: IO()
 cadastroAluno :: IO()
 escolherCadastro :: String -> IO()
 escolherOpcaoCadastro :: IO()
 cadastroGeral :: IO()
+
+data Disciplina = Disciplina {
+    nome :: String,
+    nomeProfessor :: String,
+    matriculaProfessor :: String,
+    senha :: String
+} deriving (Generic, Show)
+
+data Aluno = Aluno {
+    nome :: String,
+    matricula :: String,
+    senha :: String
+} deriving (Generic, Show)
+
+instance ToJSON Disciplina
+instance ToJSON Aluno
 
 cadastroGeral = do
     putStrLn "CADASTRO ====================="
@@ -37,11 +60,8 @@ cadastroProfessor = do
     nomeDaDisciplina <- getLine
     putStrLn "Cadastro concluído!"
     putStrLn " "
-    writeFile ("../db/professor/" ++ matricula ++ ".txt") (
-        "nome: " ++ nome ++ "\n" ++
-        "matricula: " ++ matricula ++ "\n" ++
-        "senha: " ++ senha ++ "\n" ++
-        "disciplina: " ++ nomeDaDisciplina ++ "")
+    let dados = encode (Disciplina {nome = nomeDaDisciplina, nomeProfessor = nome, matriculaProfessor = matricula, senha = senha})
+    B.writeFile ("./db/disciplina/" ++ nomeDaDisciplina ++ ".json") dados
 
 cadastroAluno = do
     putStrLn "CADASTRO DE ALUNO"
@@ -53,8 +73,6 @@ cadastroAluno = do
     senha <- getLine
     putStrLn "Cadastro concluído!"
     putStrLn " "
-    writeFile ("../db/aluno/" ++ matricula ++ ".txt") (
-        "nome: " ++ nome ++ "\n" ++
-        "matricula: " ++ matricula ++ "\n" ++
-        "senha: " ++ senha ++ "")
+    let dados = encode (Aluno {nome = nome, matricula = matricula, senha = senha})
+    B.writeFile ("./db/aluno/" ++ matricula ++ ".json") dados
     
