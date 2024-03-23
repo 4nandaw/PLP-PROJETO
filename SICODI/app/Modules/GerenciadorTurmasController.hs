39120430ef441a118f731f3e02d9e09be60e7ef3
Zeroum2345 <marcuspsferreira@gmail.com>
2024-03-23 14:02:40 -0300

    ajuste nos erros de compinlacao

--- SICODI/app/Modules/GerenciadorTurmasController.hs
+++ SICODI/app/Modules/GerenciadorTurmasController.hs
@@ -1,6 +1,7 @@
 module Modules.GerenciadorTurmasController where
 
 import Modules.GerenciadorTurmas
+import System.Directory
 
 opcoesDeTurmas :: String -> IO()
 opcoesDeTurmas disciplina = do
@@ -22,25 +23,82 @@ escolherOpcaoTurma disciplina = do
     if (escolha /= "0") then opcoesDeTurmas disciplina
     else putStrLn " "
 
-listarTurmasController :: IO ()    
+listarTurmasController :: String -> IO()    
 listarTurmasController disciplina = do
     putStrLn ("Turmas de " ++ disciplina)
 
-    putStrLn $ listarTurmas disciplina
+    response <- listarTurmas disciplina
+    putStrLn response
 
     putStrLn "==============================================="
     putStrLn "Informe um codigo de turma, ou ENTER para sair:"
     codigo <- getLine
 
-    if codigo /= "" then verAlunos (diretorio ++ codigo ++ "/alunos/")
+    let diretorio = "./db/disciplinas/" ++ disciplina ++ "/turmas/"
+
+    if codigo /= "" then do
+        validarTurma <- doesFileExist (diretorio ++ codigo ++ codigo ++ ".json") 
+
+        if not validarTurma then putStrLn "Codigo de turma invalido"
+        else do 
+            responseAlunos <- verAlunos (diretorio ++ codigo ++ "/alunos/")
+            putStrLn responseAlunos
+
     else putStrLn "" 
+
+criarTurmaController :: String -> IO()
+criarTurmaController disciplina = do
+    putStrLn "CADASTRO DE TURMA"
+    putStrLn "Nome da turma: "
+    nome <- getLine
+    putStrLn "Codigo da turma: "
+    codigo <- getLine
+
+    response <- criarTurma disciplina nome codigo
+    putStrLn response
+
+solicitarEAlocarAlunoController :: String -> IO()
+solicitarEAlocarAlunoController disciplina = do
+    putStrLn "Informe o codigo da turma: "
+    codigo <- getLine
+
+    response <- solicitarEAlocarAluno disciplina codigo
+
+    if response /= "Codigo invalido!" then do
+        putStrLn "Informe a matricula: "
+        m <- getLine
+        alocarAluno m disciplina codigo
+    else putStrLn ""
+
+excluirAlunoController :: String -> IO()
+excluirAlunoController disciplina = do
+    putStrLn "Informe o codigo da turma: "
+    codigo <- getLine
+    
+    response <- excluirAluno disciplina codigo
+
+    if response /= "Truma invalida!" then do 
+        putStrLn "Informe a matricula do aluno: "
+        matricula <- getLine
+
+        responseAluno <- removerAluno disciplina matricula codigo
+        putStrLn responseAluno
+    else putStrLn ""
+
+excluirTurmaController :: String -> IO()
+excluirTurmaController disciplina = do
+    putStrLn "Informe o codigo da turma a ser excluida: "
+    codigo <- getLine
+
+    response <- excluirTurma disciplina codigo
+    putStrLn response
 
 escolherOpcaoMenuTurmas :: String -> String -> IO()
 escolherOpcaoMenuTurmas escolha disciplina
         | (escolha == "0") = putStrLn " "
         | (escolha == "1") = listarTurmasController disciplina
-        | (escolha == "2") = criarTurma disciplina
-        | (escolha == "3") = solicitarEAlocarAluno disciplina
-        | (escolha == "4") = excluirAluno disciplina
-        | (escolha == "5") = excluirTurma disciplina
+        | (escolha == "2") = criarTurmaController disciplina
+        | (escolha == "3") = solicitarEAlocarAlunoController disciplina
+        | (escolha == "4") = excluirAlunoController disciplina
+        | (escolha == "5") = excluirTurmaController disciplina
         | otherwise = putStrLn "Opção Inválida!!"
