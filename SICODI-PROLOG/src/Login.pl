edac3ba0078069d96325888a29cc9ba3f1875630
Ananda Vilar Vidal <ananda.vilar.vidal@ccc.ufcg.edu.br>
2024-04-27 16:16:22 -0300

    feat: disciplina menu

--- SICODI-PROLOG/src/Login.pl
+++ SICODI-PROLOG/src/Login.pl
@@ -2,9 +2,7 @@
 :- use_module(library(http/json)).
 :- use_module("../utils/Utils").
 
-
 % LOGIN ALUNOS
-
 login_aluno :-
     write("Digite a matrícula do aluno: "), nl,
     read(Matricula), nl,
@@ -44,18 +42,18 @@ login_disciplina :-
 
         open(Path, read, Stream),
         json_read_dict(Stream, Dict),
-        ((Dict.senha =:= Senha) -> write('Login realizado!!!') ; write('Senha incorreta! \n')),
-        close(Stream)
+        ((Dict.senha =:= Senha) -> write('Login realizado com sucesso!\n') ; write('Senha incorreta!\n')),
+        close(Stream),
+        disciplina_menu(Disciplina)
     ) ;  write('Disciplina não existe').
 /* % shell(clear) */
 
-
 login_menu:-
-    write("============== LOGIN ==============="),
-    write("
-    [0] Sair \n
-    [1] Login Disciplina \n
-    [2] Login Aluno "),
+    write("\nLOGIN ======================\n"),
+    write("[0] Voltar ao menu principal\n"),
+    write("[1] Login de professor\n"),
+    write("[2] Login de aluno\n"),
+    write("=============================\n"),
     read(Opcao),
     escolher_opcao_login(Opcao), !.
     /*% shell(clear) Essa linha só irá funcionar em Mac ou linux/windows essa função n vai pegar*/
@@ -63,3 +61,25 @@ login_menu:-
 escolher_opcao_login(1):- login_disciplina, !.
 escolher_opcao_login(2):- login_aluno, !.
 escolher_opcao_login(_):- nl, write("ENTRADA INVÁLIDA"), nl.
+
+disciplina_menu(Disciplina):-
+    string_upper(Disciplina, X),
+    write("\nMENU DE "), write(X), write(" ========\n"),
+    write("Digite uma opção: \n"),
+    write("[0] Voltar\n"),
+    write("[1] Minhas turmas\n"),
+    write("[2] Criar turma\n"),
+    write("[3] Adicionar aluno\n"),
+    write("[4] Excluir aluno\n"),
+    write("[5] Excluir turma\n"),
+    write("====================\n"),
+    read(Opcao),
+    escolher_opcao_disciplina_menu(Opcao), !.
+
+escolher_opcao_disciplina_menu(0):- main, !.
+escolher_opcao_disciplina_menu(1):- write("Em construção").
+escolher_opcao_disciplina_menu(2):- write("Em construção").
+escolher_opcao_disciplina_menu(3):- write("Em construção").
+escolher_opcao_disciplina_menu(4):- write("Em construção").
+escolher_opcao_disciplina_menu(5):- write("Em construção").
+escolher_opcao_disciplina_menu(_):- nl, write("ENTRADA INVÁLIDA"), nl.
