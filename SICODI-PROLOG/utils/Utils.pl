fbfa9f74f04fb4469d169d96565c124aa3cc7269
Jose-Jardel-Alves <jose.jardel.alves70@gmail.com>
2024-04-28 23:41:05 -0300

    US3 finalizada

--- SICODI-PROLOG/utils/Utils.pl
+++ SICODI-PROLOG/utils/Utils.pl
@@ -1,7 +1,8 @@
 :- module(utils, [validando_dados/3,
 write_json/2,
 read_json/2, 
-not_exists_file/1]).
+not_exists_file/1,
+convert_to_string/2]).
 
 :- use_module(library(http/json)).
 
@@ -22,3 +23,17 @@ write_json(Path, D):-
     open(Path, write, Stream),
     json_write_dict(Stream, D),
     close(Stream).
+
+convert_to_string(Data, String) :-
+    (
+        atom(Data) ->
+        atom_string(Data, String)
+    ;
+        number(Data) ->
+        atom_string(Data, String)
+    ;
+        string(Data) ->
+        String = Data
+    ;
+        term_string(Data, String)
+    ).
